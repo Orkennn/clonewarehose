f913a3ed53b3df4ff2d2a8d1e69a5983df203392
Orkennn <sabit_orken@mail.ru>
2024-09-26 20:30:48 +0500

    Jinja2 added

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import uvicorn
-from fastapi import FastAPI
 
 from core.settings import settings
 from api import router as router_v1
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
 
 app = FastAPI()
 
 app.include_router(router=router_v1, prefix=settings.api_prefix)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
