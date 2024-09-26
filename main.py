2e79d5b88b8f5c4bdde2b6ef9fa78e111567cb13
Orkennn <sabit_orken@mail.ru>
2024-09-26 21:34:16 +0500

    added new handler

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi import Request
+from api.orders.views import create_order
 
 app = FastAPI()
 
@@ -15,12 +16,18 @@ app.include_router(router=router_v1, prefix=settings.api_prefix)
 templates = Jinja2Templates(directory="templates")
 
 
+@app.post("/orders", response_class=HTMLResponse)
+async def submit_order(request: Request):
+    form_data = await request.json()
+    return await create_order(order_in=form_data)
+
+
 @app.exception_handler(Exception)
 async def global_exception_handler(request: Request, exc: Exception):
     return templates.TemplateResponse("error.html", {"request": request, "error": str(exc)})
 
 
-@app.post("/", response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
