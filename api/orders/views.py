c82cf0d92b8118448987dc243888e7c17926653d
Orkennn <sabit_orken@mail.ru>
2024-09-26 21:21:36 +0500

    fixed some bugs

--- api/orders/views.py
+++ api/orders/views.py
@@ -4,22 +4,8 @@ from core.models import db_helper
 from . import crud
 from .schemas import OrderCreate, Order
 from .dependencies import order_by_id
-from fastapi.responses import HTMLResponse
-from fastapi import Request
-
-from ...main import templates
 
 router = APIRouter(tags=["Orders"])
-
-@router.post("/", response_class=HTMLResponse)
-async def create_order_view(
-    request: Request,
-    order_in: OrderCreate,
-    session: AsyncSession = Depends(db_helper.session_dependency)
-):
-    order = await crud.create_order(session=session, order_create=order_in)
-
-    return templates.TemplateResponse("index.html", {"request": request, "order": order})
 
 
 @router.post("/orders", response_model=Order, status_code=status.HTTP_201_CREATED)
